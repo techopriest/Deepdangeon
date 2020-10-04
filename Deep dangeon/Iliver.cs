2eae75ccfdfbd5947184b69e8f36ea34d1f95d6b
techopriest <65525066+techopriest@users.noreply.github.com>
2020-10-04 20:47:39 +1000

    Add files via upload
    да как он работает то гит этот ваш

---
+++ Deep dangeon/Iliver.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Deep_dangeon
+{
+    interface ILiver
+    {
+        int MaxHeal { get; set; }
+        int Damage { get; set; }
+        int Heal { get; set; }
+    }
+}
---
+++ Deep dangeon/Map.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Deep_dangeon
+{
+    class Map
+    {
+        public int[,] dangeonMap = new int[3, 4] { { 0, 0, 0, 0 }, { 2, 1, 2, 0 }, { 0, 0, 0, 0 } };
+
+        public void ChooseRoom()
+        {
+
+            Console.WriteLine("Choose next room : ");
+
+        }
+
+    }
+}
